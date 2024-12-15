5a85d0dc5b1e4d728969536a438c755f759d9e7d
DragonaCB <dragona163@bk.ru>
2024-12-16 01:26:31 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        sc.close();
         try {
             System.out.println(calc(input));
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            sc.close();
-        }
+        } 
     }
 
     public static String calc(String input) {
